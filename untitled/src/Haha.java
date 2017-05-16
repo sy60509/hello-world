6252da8c21d9309e2ba6355c66bec8c2efd23eec
sy60509 <sy60509@gmail.com>
2017-05-16 14:09:47 +0800

    Add files via upload

---
+++ untitled/src/Haha.java
@@ -0,0 +1,17 @@
+import java.io.Console;
+import java.lang.Math;
+import java.util.Scanner;
+
+public class Haha {
+
+    public static void main(String[] args){
+            System.out.println("Hello world");
+        }
+
+    }
+
+
+
+
+
+
